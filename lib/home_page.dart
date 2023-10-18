e947bee160fba892e117ea622a4ceea19662e3ca
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-10-18 13:22:03 +0700

    🔨 refactor(home_page.dart): refactor HomePage widget to use StatefulWidget for better state management 🔨 refactor(home_page.dart): move counter logic to _HomePageState and add timer for periodic counter update 🔨 refactor(home_page.dart): update AppBar title to "State Management Practice" for better clarity 🔨 refactor(home_page.dart): update ElevatedButton onPressed to call updateCounter method for counter update 🔨 refactor(home_page.dart): update SizedBox width to use MediaQuery for responsive design 🔨 refactor(home_page.dart): update SizedBox height to use explicit value for better control 🔨 refactor(home_page.dart): update Text style for counter to use explicit fontSize and fontWeight 🔨 refactor(home_page.dart): update SizedBox height for spacing between counter and button 🔨 refactor(home_page.dart): update SizedBox width to use MediaQuery for responsive design 🔨 refactor(home_page.dart): update ElevatedButton onPressed to call updateCounter method for counter update 🔨 refactor(home_page.dart): update ElevatedButton child text to "Update Counter" for better clarity 🔨 refactor(main.dart): import HomePage from home_page.dart for better modularity 🔨 refactor(main.dart): update home property of MaterialApp to use HomePage widget for initial route

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,28 +1,79 @@
 // ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int counter = 0;
+  ValueNotifier<int> counter2 = ValueNotifier<int>(0);
+  late Timer timer;
+
+  void updateCounter() {
+    counter++;
+    print('Counter: $counter');
+    // & setState just rebuild UI / Widget Once as it is called
+    setState(() {});
+  }
+
+  void updateConterObservable(params) {
+    
+  }
+
+  @override
+  void initState() {
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) { });
+    
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    timer.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Dashboard"),
+        title: const Text("State Management Practice"),
         actions: const [],
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: 40,
-            child: ElevatedButton(
-              onPressed: () async {},
-              child: const Text("Dialog"),
-            ),
+      body: SingleChildScrollView(
+        controller: ScrollController(),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20.0),
+              Text(
+                counter.toString(),
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: () => updateCounter(),
+                  child: const Text("Update Counter"),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_statemanagement1/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,14 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter State Management'),
-        ),
-        body: const Center(
-          child: Text("Let's Learn"),
-        ),
-      ),
+      home: const HomePage(),
     );
   }
 }
